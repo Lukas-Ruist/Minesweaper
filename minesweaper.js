137dfe714fa78322a6e5ad5867d6b2b543009591
Lukas-Ruist <lukas.ruist07@skola.stenungsund.se>
2026-03-18 11:28:57 +0100

    jag lade till flagga som variabel och optimerade array erna

--- minesweaper.js
+++ minesweaper.js
@@ -5,17 +5,15 @@ let x_max = 9
 let minor_max
 let z = 0
 let bomb = false 
+let flagga = false
 
 function skaparutnät(y_max, x_max) {
     for (let x = 1; x <= y_max; x++) {
 
-        let rad = []
-
         for (let y = 1; y <= x_max; y++) {
-            rad.push([x, y, z, bomb])
+            rutnat.push([x, y, z, bomb, flagga])
         }
 
-        rutnat.push(rad)
     }
             return rutnat
 
@@ -24,3 +22,9 @@ function skaparutnät(y_max, x_max) {
 
 
 console.log(skaparutnät(y_max,x_max))
+
+
+
+function kompisstatu(x,y,){
+    
+}
