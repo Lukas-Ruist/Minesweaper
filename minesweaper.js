87d13705aa8f55eefff715c367efbcca770a9d98
Lukas-Ruist <lukas.ruist07@skola.stenungsund.se>
2026-03-19 11:51:46 +0100

    jag la till en lägg in bomb, öppna rutan, varna gränsande rutor att det fins en bomb, m.m

--- minesweaper.js
+++ minesweaper.js
@@ -1,30 +1,82 @@
-
 let rutnat = []
 let y_max = 9
 let x_max = 9
 let minor_max
-let z = 0
+let argakompisar = 0
 let bomb = false 
 let flagga = false
+let opnad = false
+let antal_bomber = 0
 
-function skaparutnät(y_max, x_max) {
-    for (let x = 1; x <= y_max; x++) {
 
-        for (let y = 1; y <= x_max; y++) {
-            rutnat.push([x, y, z, bomb, flagga])
+// skapar rutnät med rutor [x, y, kompisar som är varnade,om den är en bomb, om den är flaggad]
+function skaparutnät(x_max, y_max) {
+    rutnat = []
+
+    for (let y = 0; y < y_max; y++) {
+
+        let rad = []; 
+
+        for (let x = 0; x < x_max; x++) {
+            rad.push([x, y, argakompisar, bomb, flagga, opnad]); 
         }
 
+        rutnat.push(rad);
     }
-            return rutnat
+
+    return rutnat;
+}
+
+function skapabomb(y, x){
+
+        if(rutnat[y][x][3] === false){
+
+        rutnat[y][x][3] = true
+        antal_bomber++
+
+    }
 
 }
 
+// ökar antal kompisar runt varje bomb
+function kompisvarnare(y_max, x_max) {
+    for (let y = 0; y < y_max; y++) {
+
+        for (let x = 0; x < x_max; x++) {
+
+            if (rutnat[y][x][3]) {
+
+                if (y+1 < y_max) {
+                    rutnat[y+1][x][2] += 1;
+                    if (x-1 >= 0) rutnat[y+1][x-1][2] += 1;
+                    if (x+1 < x_max) rutnat[y+1][x+1][2] += 1;
+                }
+
+                if (y-1 >= 0) {
+                    rutnat[y-1][x][2] += 1;
+                    if (x+1 < x_max) rutnat[y-1][x+1][2] += 1;
+                    if (x-1 >= 0) rutnat[y-1][x-1][2] += 1;
+                }
+
+                if (x-1 >= 0) rutnat[y][x-1][2] += 1;
+                if (x+1 < x_max) rutnat[y][x+1][2] += 1;
+
+            }
+        }
+    }
+}
 
 
-console.log(skaparutnät(y_max,x_max))
+// sätter en flagga på en ruta
+function flagning(y, x){
+     rutnat[y][x][4] = true
+}
 
 
+function öppning(y, x){
 
-function kompisstatu(x,y,){
-    
+    if(rutnat[y][x][3] === true){
+        //gaem over//
+    }
+     rutnat[y][x][5] = true
 }
